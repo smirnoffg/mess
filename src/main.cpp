dcc6de6787a5fbe22fe7e19acfabd3a79e3d0b58
Gleb Smirnov <smirnovg@Glebs-MacBook-Pro.local>
2023-11-15 17:01:25 +0100

    none

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <uxhw.h>
+
+int main(int argc, char *  argv[])
+{
+	double	a, b, c;
+
+	a = UxHwDoubleUniformDist(0.5, 1.0);
+	printf("a = %lf\n", a);
+
+	b = UxHwDoubleUniformDist(10.0, 20.0);
+	printf("b = %lf\n", b);
+
+	c = (a+b)/(a-b);
+	printf("c = %lf\n", c);
+
+	return 0;
+}
